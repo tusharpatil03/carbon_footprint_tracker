7a7c44aac3c64d5c16ed7bd05c1531ad895ba2b8
tusharpatil03 <tusha2149@gmail.com>
2025-08-30 12:47:36 +0530

    feature: login and signup

---
+++ server/src/controllers/user/login.ts
@@ -0,0 +1,60 @@
+import { SignupInfo } from './signup';
+import {  User } from '../../models/User';
+import HandleError from '../../utils/Error/error';
+import {
+  generateAccessToken,
+  InterfaceAccessTokenPayload,
+} from '../../utils/auth';
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const data: SignupInfo = req.body;
+
+  try {
+    const user = await User.findOne({
+      email: data.email,
+    }).select('password').lean();
+
+    if (!user) {
+      throw new HandleError('USER NOT EXIST', 'user not found', 400);
+    }
+
+    const isValidPassword: boolean = await bcrypt.compare(
+      data.password,
+      user.password,
+    );
+
+    if (!isValidPassword) {
+      throw new HandleError('Wrong Password', 'Incorrect Password', 400);
+    }
+
+    const accessTokenPayload: InterfaceAccessTokenPayload = {
+      email: data.email,
+      userId: user._id.toString(),
+    };
+
+    const accessToken = generateAccessToken(accessTokenPayload);
+
+    if (!accessToken) {
+      throw new HandleError('JWT ERROR', 'unable to create JWT tokens', 500);
+    }
+
+    res.cookie('token', accessToken, {
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+      secure: true,
+    });
+
+    res.status(200).json({
+      message: 'login success',
+      accessToken: accessToken,
+    });
+  } catch (e: unknown | HandleError) {
+    console.log(e);
+    res.status(500).json({
+      message: e,
+    });
+  }
+};
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { userRoute } from "../controllers/user/user";
 import { recomendation } from "../controllers/user/reconmendation";
+import { login } from "../controllers/user/login";
+import { signup } from "../controllers/user/signup";
 
 const router = Router();
 
 router.get("/", userRoute);
 
 router.get("/recomendation", recomendation);
+
+router.post("/signup", signup);
+
+router.post("/login", login);
 
 export default router;
